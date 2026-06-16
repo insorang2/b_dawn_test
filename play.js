1614bb04164fd1d6feaeb8e26c52461f8629d004
insorang2 <insorang@gmail.com>
2026-06-16 17:27:01 +0900

    Update play.js

--- play.js
+++ play.js
@@ -1,23 +1,10 @@
 document.getElementById("forPuzzle").innerHTML = `
 <img
-    id="puzzleImage"
     src="images/puzzle.png"
     style="
         width:90%;
         max-width:900px;
         margin-top:30px;
-        display:none;
     "
 >
 `;
-
-const hintBtn = document.getElementById("hintBtn");
-const puzzleImage = document.getElementById("puzzleImage");
-
-hintBtn.addEventListener("click", () => {
-    puzzleImage.style.display = "block";
-
-    setTimeout(() => {
-        puzzleImage.style.display = "none";
-    }, 3000);
-});
