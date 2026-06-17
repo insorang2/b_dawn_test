caaba5e4b38852308b71e0ad784b93e8d86af7c3
insorang2 <insorang@gmail.com>
2026-06-17 20:18:49 +0900

    Update play.js

--- play.js
+++ play.js
@@ -140,7 +140,7 @@ document.addEventListener("mouseup", function(){
     const distanceY =
         Math.abs(currentY - targetY);
 
-    if(distanceX < 10 && distanceY < 10){
+    if(distanceX < 8 && distanceY < 8){
 
         draggedPiece.style.position =
             "absolute";
