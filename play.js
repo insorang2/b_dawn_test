bd4cadb47effceef27c43d7b910049afdf3cb404
insorang2 <insorang@gmail.com>
2026-06-17 14:41:02 +0900

    Update play.js

--- play.js
+++ play.js
@@ -1,1 +1,18 @@
-alert("play.js 실행됨");
+const canvas = document.getElementById("puzzleCanvas");
+const ctx = canvas.getContext("2d");
+
+const image = new Image();
+
+image.src = "images/puzzle.png";
+
+image.onload = function() {
+
+    ctx.drawImage(
+        image,
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+};
