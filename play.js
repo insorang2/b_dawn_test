13518f97f3ec5c6aa1b1e63170cae49e75e92e85
insorang2 <insorang@gmail.com>
2026-06-17 15:37:33 +0900

    clean up puzzle test code

--- play.js
+++ play.js
@@ -1,62 +1,23 @@
-const canvas = document.getElementById("puzzleCanvas");
-const ctx = canvas.getContext("2d");
+document.getElementById("forPuzzle").innerHTML = `
+<img
+    id="puzzleImage"
+    src="images/puzzle.png"
+    style="
+        width:90%;
+        max-width:900px;
+        display:none;
+    "
+>
+`;
 
-const image = new Image();
+document.getElementById("hintBtn").onclick = function(){
 
-image.src = "images/puzzle.png";
+    const image =
+        document.getElementById("puzzleImage");
 
-image.onload = function(){
+    image.style.display = "block";
 
-    createPieces();
+    setTimeout(function(){
+        image.style.display = "none";
+    },3000);
 };
-
-function createPieces(){
-
-const piecesDiv =
-    document.getElementById("pieces");
-
-piecesDiv.innerHTML = "";
-
-const rows = 10;
-const cols = 10;
-
-const pieceWidth = image.width / cols;
-const pieceHeight = image.height / rows;
-
-console.log(pieceWidth, pieceHeight);
-
-for(let y=0; y<rows; y++){
-
-    for(let x=0; x<cols; x++){
-
-        const piece =
-            document.createElement("canvas");
-
-        piece.width = pieceWidth;
-        piece.height = pieceHeight;
-
-        piece.className = "piece";
-
-        const pctx =
-            piece.getContext("2d");
-
-        pctx.drawImage(
-            image,
-            x * pieceWidth,
-            y * pieceHeight,
-            pieceWidth,
-            pieceHeight,
-            0,
-            0,
-            pieceWidth,
-            pieceHeight
-        );
-
-        piece.style.order =
-            Math.floor(Math.random() * 1000);
-
-        piecesDiv.appendChild(piece);
-    }
-}
-
-}
